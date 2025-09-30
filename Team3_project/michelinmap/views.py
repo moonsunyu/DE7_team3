b9c1722f502a1e9b15d8aba104cd2c78b051a09e
Minji-Ko-68 <kominji68@gmail.com>
2025-09-30 19:00:19 +0900

    feat:모델수정,리스트구현

--- Team3_project/michelinmap/views.py
+++ Team3_project/michelinmap/views.py
@@ -4,8 +4,23 @@ from .models import *
 from django.views.decorators.clickjacking import xframe_options_exempt
 
 def index(request):
-    return render(request, "michelinmap/index.html")
+    regions = list(Region.objects.values('id', 'r_name'))
+    cities = list(City.objects.values('id', 'c_name', 'region_id'))
 
+    city_id = request.GET.get('city_id')
+    if city_id:
+        restaurants = list(Restaurant.objects.filter(city_id=city_id)
+                           .values('id', 'name', 'address', 'city_id'))
+    else:
+        restaurants = list(Restaurant.objects.values('id', 'name', 'address', 'city_id'))
+
+    context = {
+        'regions': regions,
+        'cities': cities,
+        'restaurants': restaurants,
+        'selected_city_id': int(city_id) if city_id else None
+    }
+    return render(request, 'michelinmap/index.html', context)
 
 @xframe_options_exempt
 def map_view(request):

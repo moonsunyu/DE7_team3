50d08f9550d785156e497518504f7110543e424d
JeongJaeHyeon <zhfem05@gmail.com>
2025-09-30 15:29:03 +0900

    뼈대

---
+++ Team3_project/michelinmap/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+
+admin.site.register(Restaurant)
+admin.site.register(Review)
+admin.site.register(Region)
+admin.site.register(City)
